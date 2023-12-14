0b3ca427f63ba44c06af7637bef1d51c2cb2d7f7
Akansha <60355616+Akansha887731@users.noreply.github.com>
2023-12-15 00:43:27 +0530

    Update Valid_sudoku.py

--- Valid_sudoku.py
+++ Valid_sudoku.py
@@ -42,6 +42,26 @@ board[i].length == 9
 board[i][j] is a digit 1-9 or '.'.
 """
 
+"""
+Please upvote if you like the solution :)
+Intuition
+The code checks if a given Sudoku board is valid or not. A valid Sudoku board must satisfy the following conditions:
+
+Each row must have unique values from 1 to 9.
+Each column must have unique values from 1 to 9.
+Each of the 9 subgrids (3x3) must have unique values from 1 to 9.
+
+Approach
+The code uses sets to keep track of the values in each row, column, and subgrid. It iterates through each cell in the Sudoku board, checking whether the current value violates any of the three conditions mentioned above. If it finds a violation, it returns False; otherwise, it adds the value to the corresponding sets for the row, column, and subgrid. If the entire board is traversed without finding any violations, the function returns True.
+
+Complexity
+Time complexity:
+Let's denote the size of the Sudoku board as N (which is 9 in this case). The code iterates through each cell in the N x N board, performing constant-time operations for each cell. Therefore, the time complexity is O(N^2).
+
+Space complexity:
+The space complexity is O(N^2) as well. The code uses sets to store values for each row, column, and subgrid. The sizes of these sets are proportional to the size of the board, and since there are three types (row, column, subgrid), the space complexity is O(N^2).
+"""
+
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
